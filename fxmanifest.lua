dc551c88e375fbbe4f0f6aaa5d8bbfa40715f50b
haukeeem <malliekgr@gmail.com>
2025-01-07 18:11:15 -0600

    add resource files

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+-- Basic Info
+fx_version 'bodacious'
+game 'gta5'
+
+-- Resource Metadata
+name 'UCI Framework Core'
+description 'Contains core materials for the overall UCI Framework bundle.' 
+author 'London Rials'
+version '1.0.0'
+
+-- Client Side Scripts
+client_scripts {
+    'client/client.lua'
+}
+
+-- Server Side Scripts
+server_scripts {
+    'server/server.lua'
+}
+
+-- Shared Scripts
+shared_scripts {
+    'shared/shared.lua'
+}
