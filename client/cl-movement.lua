50ce477e6063ea8a2c916cfecbf2f76cc7bc52da
haukeeem <malliekgr@gmail.com>
2025-01-08 18:09:14 -0600

    Created Client Movement

---
+++ client/cl-movement.lua
@@ -0,0 +1,15 @@
+--[[
+  _    _  _____ _____   ______                                           _       _____               
+ | |  | |/ ____|_   _| |  ____|                                         | |     / ____|              
+ | |  | | |      | |   | |__ _ __ __ _ _ __ ___   _____      _____  _ __| | __ | |     ___  _ __ ___ 
+ | |  | | |      | |   |  __| '__/ _` | '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |    / _ \| '__/ _ \
+ | |__| | |____ _| |_  | |  | | | (_| | | | | | |  __/\ V  V / (_) | |  |   <  | |___| (_) | | |  __/
+  \____/ \_____|_____| |_|  |_|  \__,_|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\  \_____\___/|_|  \___|
+                                                                                                  
+This the client side script for improving player movement to be more real.
+Make sure to restart the resource after making changes to this file to ensure the changes are live.
+/stop uciFramework-core -> /start uciFramework-core
+
+]]-------------------------------------------------------------------------------------------------------
+
+-- Load configuration
